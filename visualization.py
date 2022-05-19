6a56d6c841aa9b5f2f82f16188834eb60f9b9fb2
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-05-20 02:24:18 +0700

    Change visualization to daily report

--- visualization.py
+++ visualization.py
@@ -1,15 +1,12 @@
-from unicodedata import name
-from jinja2 import Undefined
 import streamlit as st
 import pandas as pd
 import altair as alt
-from datetime import datetime
 
 # reload on csv update
 # https://docs.streamlit.io/library/api-reference/control-flow/st.experimental_rerun
 
 '''
-# Thailand PM2.5 Forecasting 
+# Thailand PM2.5 Report
 '''
 # Constants and cached functions are here
 
@@ -24,20 +21,16 @@ stations = {
 visible_stations = []
 
 @st.cache(ttl=1800)
-def load_data(station, item_freq='hourly'):
-    return pd.read_csv('viz_source/' + station + '_' + item_freq + '.csv', parse_dates=True)
+def load_data(station):
+    return pd.read_csv('viz_source/' + station + '_hourly.csv', parse_dates=True)
 
 @st.cache(ttl=1800)
-def get_latest_data_item(df_hourly):
-    return df_hourly.iloc[[-1]].reset_index().drop('index', axis=1)
+def get_data_item(df_hourly, iloc):
+    return df_hourly.iloc[[iloc]].reset_index().drop('index', axis=1)
 
 @st.cache(ttl=1800)
-def timestamp_to_datetime(timestamp):
-    return pd.to_datetime(timestamp).to_pydatetime()
-
-@st.cache(ttl=1800)
-def date_to_datetime(date):
-    return datetime(date.year, date.month, date.day, 1,0)
+def timestamp_to_date(timestamp):
+    return pd.to_datetime(timestamp).date()
 
 @st.cache(ttl=1800)
 def get_level(x):
@@ -67,44 +60,103 @@ with st.sidebar:
     for i in range(4):
         st.color_picker(label=aqi_levels[i], value=aqi_colors[i], disabled=True)
 
-df_hourly = load_data('75t', 'hourly')
-df_weekly = load_data('75t', 'weekly')
+# TODO : Load all 3 files for 3 locations into a dict of df
+# TODO : Writeup
+# df_hourly = dict()
+df_hourly = load_data('75t')
 
+# remove
 message = 'Selected locations:'
 for item in visible_stations:
     message += ' ' + stations[item]
 message
 
-st.write('## Latest prediction:')
-latest_data = get_latest_data_item(df_hourly)
-latest_data
+latest_date = get_data_item(df_hourly, -1)['Datetime'][0]
+st.write('#### Latest prediction: ' + latest_date)
+
+for s in visible_stations:
+    first_data = get_data_item(df_hourly, 0)
+    latest_data = get_data_item(df_hourly, -1)
+    latest_data2 = get_data_item(df_hourly, -2)
+
+    # for s in visible_stations:
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label='Location', value=stations['75t'])
+    col2.metric(
+        label='PM2.5', 
+        value=(str(round(latest_data['PM2_5'][0], 2)) + ' µg/m3'), 
+        delta=round(latest_data['PM2_5'][0] - latest_data2['PM2_5'][0], 2),
+        delta_color="inverse"
+    )
+    col3.metric(
+        label='US AQI',
+        value=latest_data['AQI'][0]
+    )
 
 '''
-## PM2.5 all year round
+## PM2.5 in the Last 24 Hours
 '''
-# add graph title
+for s in visible_stations:
+    base = alt.Chart(df_hourly.iloc[-24:]).encode(
+        x=alt.X('Datetime:T', title='Date', axis=alt.Axis(format='%e %b %Y %H:%M', grid=True)),
+        y=alt.Y('PM2_5:Q', title='Mean PM2.5 (µg/m3)'),
+    ).properties(
+        title=stations[s]
+    )
 
-base1 = alt.Chart(df_hourly.iloc[-24:]).encode(
-    x=alt.X('Datetime:T', title='Date', axis=alt.Axis(format='%e %b %y %H:%M', grid=True)),
-    y=alt.Y('PM2_5:Q', title='Mean PM2.5 (µg/m3)'),
-    color=alt.Color('AQI:O', scale=alt.Scale(domain=aqi_levels, range=aqi_colors))
-)
+    history24h_chart = base.mark_bar(width=15).encode(
+        color=alt.Color('AQI:O', scale=alt.Scale(domain=aqi_levels, range=aqi_colors))
+    )
+    history24h_labels = base.mark_text(align='center', baseline='bottom', dy=-10, color='slategray').encode(
+        text=alt.Text('PM2_5:O', format=',.1f')
+    )
+    st.altair_chart(history24h_chart + history24h_labels, use_container_width=True)
 
-history24h_chart = base1.mark_bar(width=15)
-history24h_labels = base1.mark_text(baseline='top').encode(text=alt.Text('PM2_5:O', format=',.1f'))
-st.altair_chart(history24h_chart + history24h_labels, use_container_width=True)
+'''
+## PM2.5 by time of day
 
+'''
+date = st.date_input('Select a day:', 
+    min_value=timestamp_to_date(first_data['Datetime'][0]), 
+    max_value=timestamp_to_date(latest_data['Datetime'][0]),
+    value=timestamp_to_date(latest_data['Datetime'][0]))
+
+for s in visible_stations:
+    df_sliced = df_hourly[(pd.to_datetime(df_hourly['Datetime']).dt.date == date)]
+
+    base = alt.Chart(df_sliced).encode(
+        theta=alt.Theta('Hour:O'),
+        radius=alt.Radius('PM2_5:Q', title='Mean PM2.5 (µg/m3)'),
+    ).properties(
+        title=stations[s]
+    )
+
+    day_round_chart = base.mark_arc(stroke='white').encode(
+        color=alt.Color('AQI:O', scale=alt.Scale(domain=aqi_levels, range=aqi_colors))
+    )
+
+    day_round_chart_labels = day_round_chart.mark_text(radiusOffset=15).encode(text=alt.Text('PM2_5:O', format=',.1f'))
+    day_round_chart_hours = base.mark_text(radiusOffset=35, color='slategray').encode(text=alt.Text('Hour:O'))
+
+    st.altair_chart(day_round_chart + day_round_chart_labels + day_round_chart_hours, use_container_width=True)
 
 '''
 ## Overall Trend Chart
 Select an interval on the top graph to zoom in! 
 The colors display the Air Quality Index (AQI) level. Refer to the sidebar for the AQI legend.
 '''
-selection = alt.selection(type='interval', encodings=['x'])
+df_location = dict()
+for s in visible_stations:
+    df_location[s] = df_hourly.copy()
+    df_location[s]['Location'] = stations[s]
+df_merged = pd.concat([df_location[s] for s in visible_stations]) 
 
-base2 = alt.Chart(df_hourly).mark_line(color='slategray').encode(
+selection = alt.selection(type='interval', encodings=['x'])        
+
+base = alt.Chart(df_merged).mark_line(color='slategray').encode(
     x=alt.X('Datetime:T', title='Date', axis=alt.Axis(format='%b %y', grid=True)),
-    y=alt.Y('PM2_5:Q', title='PM2.5 (µg/m3)')
+    y=alt.Y('PM2_5:Q', title='PM2.5 (µg/m3)'),
+    color=alt.Color('Location:N', scale=alt.Scale(domain=[stations[s] for s in visible_stations], range=['slategray','salmon','mediumaquamarine']))
 ).properties(
     width=800,
     height=300
@@ -124,76 +176,10 @@ def get_AQI_visuals():
 
     return visual
 
-overall_trend_chart_zoomed = base2.encode(
+overall_trend_chart_zoomed = base.encode(
     x=alt.X('Datetime:T', scale=alt.Scale(domain=selection), title='Date', axis=alt.Axis(format='%e %b %y'))
 )
-overall_trend_chart = base2.properties(
+overall_trend_chart = base.properties(
     height=50
 ).add_selection(selection)
 st.altair_chart(overall_trend_chart & (get_AQI_visuals() + overall_trend_chart_zoomed), use_container_width=True)
-
-'''
-## PM2.5 by time of day
-Maximum, average, and minimum PM2.5 at each hour of day, calculated during the selected range.
-
-Select a date range:
-'''
-start_date = st.date_input('Start date', 
-    min_value=datetime(2020,6,2,1,0), 
-    max_value=timestamp_to_datetime(latest_data['Datetime'][0]),
-    value=datetime(2021,3,30,1,0))
-end_date = st.date_input('End date', 
-    min_value=datetime(2020,6,2,1,0), 
-    max_value=timestamp_to_datetime(latest_data['Datetime'][0]),
-    value=datetime(2021,3,31,1,0))
-
-df_sliced = df_hourly[
-    (pd.to_datetime(df_hourly['Datetime']) >= date_to_datetime(start_date)) 
-    & (pd.to_datetime(df_hourly['Datetime']) <= date_to_datetime(end_date))]
-
-df_sliced_mean = df_sliced.groupby('Hour').mean().reset_index()
-add_AQI_column(df_sliced_mean)
-
-df_sliced_min = df_sliced.groupby('Hour').min().reset_index()
-add_AQI_column(df_sliced_min)
-
-df_sliced_max = df_sliced.groupby('Hour').max().reset_index()
-add_AQI_column(df_sliced_max)
-
-day_round_chart_max = alt.Chart(df_sliced_max).mark_arc(stroke='white').encode(
-    theta=alt.Theta('Hour:O'),
-    radius=alt.Radius('PM2_5:Q', title='Mean PM2.5 (µg/m3)'),
-    color=alt.Color('AQI:O', scale=alt.Scale(domain=aqi_levels, range=aqi_colors))
-).properties(
-    title='Maximum PM2.5 at each hour of day'
-)
-
-day_round_chart_mean = alt.Chart(df_sliced_mean).mark_arc(stroke='white').encode(
-    theta=alt.Theta('Hour:O'),
-    radius=alt.Radius('PM2_5:Q', title='Mean PM2.5 (µg/m3)'),
-    color=alt.Color('AQI:O', scale=alt.Scale(domain=aqi_levels, range=aqi_colors))
-).properties(
-    title='Average PM2.5 at each hour of day'
-)
-
-day_round_chart_min = alt.Chart(df_sliced_min).mark_arc(stroke='white').encode(
-    theta=alt.Theta('Hour:O'),
-    radius=alt.Radius('PM2_5:Q', title='Mean PM2.5 (µg/m3)'),
-    color=alt.Color('AQI:O', scale=alt.Scale(domain=aqi_levels, range=aqi_colors))
-).properties(
-    title='Minimum PM2.5 at each hour of day'
-)
-
-def get_chart_labels(chart):
-    return chart.mark_text(radiusOffset=20).encode(text=alt.Text('PM2_5:O', format=',.1f'))
-
-'View graphs'
-
-if st.checkbox('Maximum PM2.5 during date range'):
-    st.altair_chart(day_round_chart_max + get_chart_labels(day_round_chart_max), use_container_width=True)
-if st.checkbox('Average PM2.5 during date range', value=True):
-    st.altair_chart(day_round_chart_mean + get_chart_labels(day_round_chart_mean), use_container_width=True)
-if st.checkbox('Minimum PM2.5 during date range'):
-    st.altair_chart(day_round_chart_min + get_chart_labels(day_round_chart_min), use_container_width=True)
-
-
